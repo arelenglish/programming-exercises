9312cb9a12bbb8a3e962dc13eebc5ee0b54b7e07
Arel English <arelenglish@gmail.com>
2016-01-18 23:41:32 -0500

    Add rotation of matrix and update string compressor
    Rotation of a matrix in ruby is made really simple becasue of the transpose method. There isn't much to do here.
    I just added a new check to the string compressor method that only compresses a string if the compression would make the string shorter.

--- ruby/string_compressor.rb
+++ ruby/string_compressor.rb
@@ -1,17 +1,30 @@
-# A method to compress a string of alphabetical characters. 
+# A method to compress a string of alphabetical characters.
 # Eg: AAAABBBGGGEEE would become A4B3G3E3
+# if the compressed string will be longer than the original
+# string, return the original string
 
 def compress_string(str)
-  compressed_string = ""
-  count = 1
-  str.each_char.with_index do |letter, index|
-    if letter == str[index+1] 
-      count += 1
-    else 
-      compressed_string << letter 
-      compressed_string << count.to_s
-      count = 1
+  # compressed string will be the uniqued string * 2 because of the count
+  comp_length = str.chars.uniq.length * 2
+
+  # check if compressed string will be shorter than input string
+  if comp_length < str.length
+    compressed_string = ""
+    count = 1
+    # iterate over the string
+    str.each_char.with_index do |letter, index|
+      # if following letter in string is same, increment the count
+      #  else add letter and count to compressed_string var
+      if letter == str[index+1]
+        count += 1
+      else
+        compressed_string << letter
+        compressed_string << count.to_s
+        count = 1
+      end
     end
+    compressed_string
+  else
+    str
   end
-  compressed_string
 end
