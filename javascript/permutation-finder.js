9b8a26ead0c0a3d16f07c70d005726c97b92ac79
Arel English <arelenglish@gmail.com>
2021-09-17 16:00:16 -0400

    Add stdin when running permutation-finder and simplify answer

--- javascript/permutation-finder.js
+++ javascript/permutation-finder.js
@@ -1,18 +1,23 @@
 // Find if a string is a permutation of the other
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-function isPermutation(str1, str2){
-  if(str1.length === str2.length){
-    var tmpArr1 = [];
-    var tmpArr2 = [];
-    for(var i = 0; i < str1.length; i++){
-      tmpArr1.push(str1[i]);
-      tmpArr2.push(str2[i]);
+readline.question(`This program checks for permutations. What's your first string? `, stringOne => {
+  readline.question("What's your second string? ", stringTwo => {
+    console.log(isPermutation(stringOne, stringTwo))
+    readline.close()
+  })
+})
+
+function isPermutation(str1, str2) {
+  if(str1.length === str2.length) {
+    var sortedString1 = str1.split('').sort().join('').toLowerCase()
+    var sortedString2 = str2.split('').sort().join('').toLowerCase()
+    if (sortedString1 === sortedString2) {
+      return "Those strings are permutations of each other! 🥳"
     }
-    return normalizeToStr(tmpArr1) === normalizeToStr(tmpArr2);
   }
-  return false;
-}
-
-function normalizeToStr(arr){
-  arr.sort().toString().toLowerCase();
+  return "Sorry. Those strings are not permutations of each other 💩";
 }
